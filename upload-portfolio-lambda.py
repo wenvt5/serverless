9eaeca787da0b07b36a4ae04b9de6437529c06cb
Ying Wen <wenyingying@hotmail.com>
2019-01-31 09:50:08 -0500

    Added StringIO to use string instead to disk to handle temp download for unzip

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,26 +2,27 @@ import boto3
 import zipfile
 import mimetypes
 from botocore.client import Config
+import StringIO #python2
+#from io import StringIO (python3)
+
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))  #without signature_version, the index.html will be downloaded instead of being openned
 
 yingying3 = s3.Bucket('yingying3')
 yingying2=s3.Bucket('yingying2')
 
-#from io import StringIO (python3)
-# the following commented lines are only for python2
-#import StringIO
-#autoDeployS3website_zip = StringIO.StringIO()
-#yingying2.download_fileobj('autoDeployS3website.zip',autoDeployS3website_zip)
 
-yingying2.download_file('autoDeployS3website.zip','c:/Users/wenyi/autoDeployS3website.zip')
+autoDeployS3website_zip = StringIO.StringIO()
+yingying2.download_fileobj('autoDeployS3website.zip',autoDeployS3website_zip)
+
+#yingying2.download_file('autoDeployS3website.zip','c:/Users/wenyi/autoDeployS3website.zip')
 
 
 
-with zipfile.ZipFile('c:/Users/wenyi/autoDeployS3website.zip') as myzip:
-    for nm in myzip.namelist():
-        print(nm)
+#with zipfile.ZipFile('c:/Users/wenyi/autoDeployS3website.zip') as myzip:
+#    for nm in myzip.namelist():
+#        print(nm)
 
-with zipfile.ZipFile('c:/Users/wenyi/autoDeployS3website.zip') as myzip:
+with zipfile.ZipFile(autoDeployS3website_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
         yingying3.upload_fileobj(obj,nm,
